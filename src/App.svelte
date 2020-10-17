477695ee44d10a0117d66a138b107c325bdbac62
mr00z <mrfrostn@gmail.com>
2020-10-17 20:50:30 +0200

    Initial setup

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import 'halfmoon/css/halfmoon-variables.min.css';
+  import Navbar from './Navbar.svelte';
+  let name: string = 'World';
+</script>
+
+<div class="page-wrapper with-navbar">
+  <!-- Content wrapper -->
+  <Navbar />
+  <main class="content-wrapper">Hello {name}</main>
+</div>
---
+++ src/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav class="navbar">
+  <ul class="navbar-nav d-none d-md-flex">
+    <!-- d-none = display: none, d-md-flex = display: flex on medium screens and up (width > 768px) -->
+    <li class="nav-item active"><a href="#" class="nav-link">Docs</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Products</a></li>
+  </ul>
+</nav>
