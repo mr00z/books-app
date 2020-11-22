9292ffc8285af92327120d2714069938781ebfa6
mr00z <mrfrostn@gmail.com>
2020-11-22 12:39:42 +0100

    Moved column separator to compnent

---
+++ src/components/Task/TaskColumnSeparator.svelte
@@ -0,0 +1,10 @@
+<style lang="scss">
+  .task-column {
+    &-separator {
+      border-left: 2px solid #444;
+      margin: 1em;
+    }
+  }
+</style>
+
+<div class="task-column-separator d-none d-lg-block" />
--- src/views/Tasks.svelte
+++ src/views/Tasks.svelte
@@ -1,19 +1,12 @@
 <script>
   import TaskColumn from '../components/Task/TaskColumn.svelte';
+  import TaskColumnSeparator from '../components/Task/TaskColumnSeparator.svelte';
   import Col from '../halfmoon/BuildingBlocks/Grid/Col.svelte';
   import Row from '../halfmoon/BuildingBlocks/Grid/Row.svelte';
   import { TaskTypes } from '../models/Task/TaskTypes';
-
-  import './Tasks.scss';
 </script>
 
 <style lang="scss">
-  .task-column {
-    &-separator{
-    border-left: 2px solid #444;
-    margin: 1em;
-    }
-  }
   :global(.full-height) {
     min-height: 100%;
   }
@@ -23,11 +16,11 @@
   <Col sm={12} lg={3} className="mx-auto">
     <TaskColumn tasksType={TaskTypes.TO_DO} />
   </Col>
-  <div class="task-column-separator" />
+  <TaskColumnSeparator />
   <Col sm={12} lg={3} className="mx-auto">
     <TaskColumn tasksType={TaskTypes.IN_PROGRESS} />
   </Col>
-  <div class="task-column-separator" />
+  <TaskColumnSeparator />
   <Col sm={12} lg={3} className="mx-auto">
     <TaskColumn tasksType={TaskTypes.DONE} />
   </Col>
